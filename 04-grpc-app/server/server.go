e0e44c9310d5d49fdc2977798e6ac04bb8845867
Magesh Kuppan <tkmagesh77@gmail.com>
2023-05-08 14:45:38 +0530

    hosting the grpc service

--- 04-grpc-app/server/server.go
+++ 04-grpc-app/server/server.go
@@ -1,27 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 
 	"github.com/tkmagesh/ibm-advgo-mar-2023/04-grpc-app/proto"
+	"google.golang.org/grpc"
 )
 
 type appService struct {
 	proto.UnimplementedAppServiceServer
 }
 
-func (*asi appService) Add(ctx context.Context, req *proto.AddRequest) (*AddResponse, error) {
+func (asi *appService) Add(ctx context.Context, req *proto.AddRequest) (*proto.AddResponse, error) {
 	x := req.GetX()
 	y := req.GetY()
 	fmt.Println("[appService.add() processing %d and %d\n", x, y)
 
 	result := x + y
 	res := &proto.AddResponse{
-		Result : result,
+		Result: result,
 	}
 	return res, nil
 }
 
 func main() {
-
+	asi := &appService{}
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	grpcServer := grpc.NewServer()
+	proto.RegisterAppServiceServer(grpcServer, asi)
+	grpcServer.Serve(listener)
 }
