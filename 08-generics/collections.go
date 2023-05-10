3830f23e8c6f2d483df5d6e9d694f4100f0a0def
Magesh Kuppan <tkmagesh77@gmail.com>
2023-05-10 14:58:25 +0530

    generics assignment

---
+++ 08-generics/collections.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Product struct {
+	Id       int
+	Name     string
+	Cost     float32
+	Units    int
+	Category string
+}
+
+// fmt.Stringer interface implementation
+func (p Product) String() string {
+	return fmt.Sprintf("Id = %d, Name = %q, Cost = %v, Units = %d, Category = %q", p.Id, p.Name, p.Cost, p.Units, p.Category)
+}
+
+type Products []Product
+
+func (products Products) String() string {
+	var builder strings.Builder
+	for _, product := range products {
+		builder.WriteString(fmt.Sprintf("%s\n", product))
+	}
+	return builder.String()
+}
+
+func (products Products) filter(predicate func(Product) bool) Products {
+	var result Products
+	for _, p := range products {
+		if predicate(p) {
+			result = append(result, p)
+		}
+	}
+	return result
+}
+
+/* Write a generic filter "function" (not a method) that can be used to filter a collection of any type*/
+
+func main() {
+	products := Products{
+		Product{105, "Pen", 5, 50, "Stationary"},
+		Product{107, "Pencil", 2, 100, "Stationary"},
+		Product{103, "Marker", 50, 20, "Utencil"},
+		Product{102, "Stove", 5000, 5, "Utencil"},
+		Product{101, "Kettle", 2500, 10, "Utencil"},
+		Product{104, "Scribble Pad", 20, 20, "Stationary"},
+		Product{109, "Golden Pen", 2000, 20, "Stationary"},
+	}
+	fmt.Println("Initial List")
+	fmt.Println(products)
+
+	var stationaryProducts = products.filter(func(p Product) bool {
+		return p.Category == "Stationary"
+	})
+	fmt.Println("Stationary Products")
+	fmt.Println(stationaryProducts)
+
+	nos := []int{3, 1, 4, 2, 5}
+	floats := []float32{4.5, 1.7, 4.8, 2.3, 5.4}
+
+}
